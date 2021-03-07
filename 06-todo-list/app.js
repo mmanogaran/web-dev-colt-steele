cd4a02271ba8691424d41a6ba182a8747c738322
mmanogaran <mathu.manogaran@gmail.com>
2021-03-07 14:50:40 -0500

    Todo List: Display list on browser (#1)
    Adds some basic styling and a dynamically updating display of the todo list on the browser.
    
    * Add initial CSS styling
    
    * Add styling for todo list display on browser
    
    * Create/ remove .todo elements dynamically
    
    * Updated README to include modifications

--- 06-todo-list/app.js
+++ 06-todo-list/app.js
@@ -3,6 +3,7 @@ setTimeout(toDoApp, 100);
 function toDoApp() {
     console.log("Welcome to Todo List");
     const todoList = [];
+    const bTodoList = document.querySelector("#todo-list");
     while (true) {
         const command = prompt("What would you like to do?");
 
@@ -11,6 +12,21 @@ function toDoApp() {
             case 'new':
                 const newTodo = prompt("Enter new todo: ");
                 if (newTodo) {
+                    const bTodo = document.createElement("article")
+                    bTodo.classList.add("todo");
+
+                    const bTodoIndex = document.createElement("div");
+                    bTodoIndex.classList.add("todo-index");
+                    bTodoIndex.innerText = todoList.length;
+                    bTodo.appendChild(bTodoIndex);
+
+                    const bTodoValue = document.createElement("div");
+                    bTodoValue.classList.add("todo-value");
+                    bTodoValue.innerText = newTodo;
+                    bTodo.appendChild(bTodoValue);
+
+                    bTodoList.appendChild(bTodo);
+
                     todoList.push(newTodo);
                     console.log(`'${newTodo}' added to list`);
                 } else {
@@ -27,6 +43,13 @@ function toDoApp() {
             case 'delete':
                 const iToDelete = parseInt(prompt("Enter index of todo to delete: "));
                 if (!Number.isNaN(iToDelete) && iToDelete >= 0 && iToDelete < todoList.length) {
+
+                    bTodoList.removeChild(bTodoList.children[iToDelete]);
+
+                    for (let i = iToDelete; i < bTodoList.childElementCount; i++) {
+                        bTodoList.children[i].firstElementChild.innerText = i;
+                    }
+
                     const toDelete = todoList.splice(iToDelete, 1);
                     console.log(`'${toDelete}' removed from list`);
                 } else {
